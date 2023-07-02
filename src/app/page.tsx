f25ad8f07517a6d289997e3963ffb76934e8893c
Denis Bugreev <Den9.rcq@gmail.com>
2023-07-02 18:19:26 +0300

    eslint config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import Link from 'next/link'
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <section className="p-10">
-      <Link className="p-2 border-2 border-gray-400 rounded-md hover:shadow" href="/dashboard">Dashboard</Link>
+      <Link className="p-2 border-2 border-gray-400 rounded-md hover:shadow" href="/dashboard">
+        Dashboard
+      </Link>
     </section>
-  )
+  );
 }
