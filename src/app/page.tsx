eeeb6aeb5ad747bf39f9a387c978d761a5eb1f00
Denis Bugreev <den9rcq@gmail.com>
2023-10-09 11:32:24 +0300

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,6 @@ import MyCarousel from '@/components/MyCarousel';
 import Marquee from '@/components/MarqueeBrands';
 import Products from '@/components/Products';
 
-// TODO: test
-
 export default function Home() {
   return (
     <>
